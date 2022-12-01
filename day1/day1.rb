b399f71cf838fec1c5c90bb2cd34baebeef10b6a
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-12-01 07:52:55 -0300

    Finished day 1

---
+++ day1/day1.rb
@@ -0,0 +1,14 @@
+#Part 1
+elves = File.read('input.txt').split("\n\n")
+#elves = File.read('test-input.txt').split("\n\n")
+
+calories = elves.map {|elf| 
+  calories = elf.split.map! {|c| c.to_i}
+  calories.sum
+}
+puts calories.max
+
+#Part 2
+calories.sort! {|a, b| b <=> a}
+puts calories[0] + calories[1] + calories[2]
+

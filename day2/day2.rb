7bbaeae0be591be197ac6304a9a4f82e23cb76f4
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-12-03 08:14:29 -0300

    Implemented day 2 using a lookup table

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,71 +1,66 @@
 input = File.readlines('input.txt').map(&:split)
 #input = File.readlines('test-input.txt').map(&:split)
 
-module RPS
-  ROCK = 1
-  PAPER = 2
-  SCISSORS = 3
-  
-  FIRST_WON = 0
-  TIE = 3
-  SECOND_WON = 6
-end
-
-wins = {
-  RPS::ROCK => RPS::SCISSORS,
-  RPS::PAPER => RPS::ROCK,
-  RPS::SCISSORS => RPS::PAPER
-}
-
-loses = {
-  RPS::ROCK => RPS::PAPER,
-  RPS::PAPER => RPS::SCISSORS,
-  RPS::SCISSORS => RPS::ROCK
-}
 
 # Part 1
 
-actions = {
-  'A' => RPS::ROCK,
-  'B' => RPS::PAPER,
-  'C' => RPS::SCISSORS,
-  'X' => RPS::ROCK,
-  'Y' => RPS::PAPER,
-  'Z' => RPS::SCISSORS
+#   | X Y Z
+# --|------
+# A | 4 8 3
+# B | 1 5 9
+# C | 7 2 6
+
+score_map = {
+  'A' => {
+    'X' => 4,
+    'Y' => 8,
+    'Z' => 3
+  },
+  'B' => {
+    'X' => 1,
+    'Y' => 5,
+    'Z' => 9
+  },
+  'C' => {
+    'X' => 7,
+    'Y' => 2,
+    'Z' => 6
+  }
 }
 
-scores = input.map {|plays|
-  first, second = plays.map {|p| actions[p]}
-  if second === first
-    result = RPS::TIE
-  elsif second === loses[first]
-    result = RPS::SECOND_WON
-  else
-    result = RPS::FIRST_WON
-  end
-  second + result 
+scores = input.map {|first, second|
+  score_map[first][second]
 }
 puts scores.sum
+
 
 # Part 2
 
-results = {
-  'X' => RPS::FIRST_WON,
-  'Y' => RPS::TIE,
-  'Z' => RPS::SECOND_WON
+#   | X Y Z
+# --|------
+# A | 3 4 8
+# B | 1 5 9
+# C | 2 6 7
+
+score_map = {
+  'A' => {
+    'X' => 3,
+    'Y' => 4,
+    'Z' => 8
+  },
+  'B' => {
+    'X' => 1,
+    'Y' => 5,
+    'Z' => 9
+  },
+  'C' => {
+    'X' => 2,
+    'Y' => 6,
+    'Z' => 7
+  }
 }
 
-scores = input.map {|first, result|
-  first = actions[first]
-  result = results[result]
-  if result === RPS::FIRST_WON
-    second = wins[first]
-  elsif result === RPS::TIE
-    second = first
-  else
-    second = loses[first]
-  end
-  second + result
+scores = input.map {|first, second|
+  score_map[first][second]
 }
 puts scores.sum
-
